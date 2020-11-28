a3d7e0f6f0cd83475aaa8a2406f696da22d20fa6
Mahdi LEKTATI <mahdi.lektati@crie.i-univ-tlse2.fr>
2020-11-28 22:43:23 +0100

    ajout du style du deck

--- tp-react/src/App.js
+++ tp-react/src/App.js
@@ -25,23 +25,28 @@ class App extends Component {
                     <span className="navbar-brand mb-0 h1">League of stones</span>
                 </nav>
                 <div className="container-fluid">
-                    <h1>Champions disponibles</h1>
                     <div class="row">
-
-                        { this.state.cards.map(card => 
-                            <div className = "col-4">
-                                <div className = "card">
-                                    <img src = { "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" +  card.key + "_0.jpg" } alt= {card.key} className="w-100 rounded card-img-top"></img>
-                                    <div className = "card-body text-center">
-                                        <h5 class="card-title text-primary">{card.key}</h5>
-                                        <hr></hr>
-                                        <p className="card-text">Attaque : {card.info.attack}</p>
-                                        <p className="card-text">Armure  : {card.info.defense}</p>
+                        <div className="col-6">
+                            <h1>Champions disponibles</h1>
+                            <div class="row">
+                                { this.state.cards.map(card => 
+                                    <div className = "col-4">
+                                        <div className = "card">
+                                            <img src = { "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" +  card.key + "_0.jpg" } alt= {card.key} className="w-100 rounded card-img-top"></img>
+                                            <div className = "card-body text-center">
+                                                <h5 class="card-title text-primary">{card.key}</h5>
+                                                <hr></hr>
+                                                <p className="card-text">Attaque : {card.info.attack}</p>
+                                                <p className="card-text">Armure  : {card.info.defense}</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                )}
                             </div>
-                        )}
-                    
+                        </div>
+                        <div className="col-6 bg-secondary">
+                            <h1 className="text-white">Mon deck</h1>
+                        </div>
                     </div>
                 </div>
             </div>
