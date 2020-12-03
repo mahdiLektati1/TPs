f1ce313db1ab8155d9a2b20c3cb3aa200e2a5cf5
Mahdi LEKTATI <mahdi.lektati@crie.i-univ-tlse2.fr>
2020-12-03 18:45:48 +0100

    ajouter composent header

---
+++ tp-react/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <span className="navbar-brand mb-0 h1">League of stones</span>
+        </nav>
+    )
+}
+
+export default Header;
