5ab05617f7391380b8c1fc1f1c209cd21ba5137e
Mahdi LEKTATI <mahdi.lektati@crie.i-univ-tlse2.fr>
2020-12-04 20:52:22 +0100

    ajout de navigation

--- tp-react-partie2/src/components/Header.js
+++ tp-react-partie2/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
   render() {
@@ -7,8 +8,13 @@ class Header extends React.Component {
          <nav className="navbar navbar-dark bg-dark">
             <span className="navbar-brand mb-0 h1">League of stones</span>
             <form className="form-inline">
-                <button className="btn btn-outline-success my-2 my-sm-0">Se connecter</button>
-                <button className="btn btn-outline-success my-2 my-sm-0 ml-2">S'inscrire</button>
+                <Link to='/signin'>
+                    <button className="btn btn-outline-success my-2 my-sm-0">Se connecter</button>
+                </Link>
+                <Link to='/signup'>
+                    <button className="btn btn-outline-success my-2 my-sm-0 ml-2">S'inscrire</button>
+                </Link>
+                
             </form>
         </nav>
       </div>
